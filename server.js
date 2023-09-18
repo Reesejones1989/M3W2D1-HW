537a486d26b4c09263cc50c90f2fff78df35e916
Reesejones1989 <mauricejonesjr@gmail.com>
2023-09-18 13:54:10 -0400

    Show view shows req.params.id

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ app.get('/pokemon/', (req,res) =>{
     res.render('Index', {pokemon: pokemon});
 })
 
-// app.get('/pokemon/')
+app.get('/pokemon/:id', (req,res) =>{
+    res.send(req.params.id);
+})
 
 
 
